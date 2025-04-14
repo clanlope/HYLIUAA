7342c75356e9e8c2c887b462ba32f98ba796a391
Clanlope <450969689@qq.com>
2025-04-14 19:36:31 +0800

    Update

---
+++ script.js
@@ -0,0 +1,42 @@
+// Function to toggle the PDF viewer
+function togglePDF() {
+  const pdfViewer = document.getElementById('pdfViewer');
+  const content = document.getElementById('content');
+  const buttonContainer = document.querySelector('.button-container');
+  const pdfFrame = document.getElementById('pdfFrame');
+
+  if (pdfViewer.classList.contains('show')) {
+    pdfViewer.classList.remove('show');
+    content.classList.remove('move');
+    buttonContainer.classList.remove('move');
+    pdfFrame.removeAttribute('src'); // Unload the PDF when hiding
+  } else {
+    pdfFrame.setAttribute('src', 'HYLIUAA.pdf#toolbar=0&navpanes=0&scrollbar=0');
+    pdfViewer.classList.add('show');
+    content.classList.add('move');
+    buttonContainer.classList.add('move');
+  }
+}
+
+const knowMoreButton = document.getElementById('knowMoreButton');
+
+knowMoreButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  document.body.classList.add('fade-out');
+  setTimeout(() => {
+    window.location.href = knowMoreButton.href;
+  }, 300); // Match the fade-out animation duration
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const kmBackButton = document.getElementById('kmBackButton');
+  if (kmBackButton) {
+    kmBackButton.addEventListener('click', (event) => {
+      event.preventDefault();
+      document.body.classList.add('fade-out');
+      setTimeout(() => {
+        window.location.href = kmBackButton.href;
+      }, 300); // Match the fade-out animation duration
+    });
+  }
+});
